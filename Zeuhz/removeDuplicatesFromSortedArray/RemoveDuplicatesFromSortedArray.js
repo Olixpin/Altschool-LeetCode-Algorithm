3a8b43a21486d2cf818fd5754a0ce01631ecd506
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-09-15 22:15:36 +0100

    Added snapshots

--- Zeuhz/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
+++ Zeuhz/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
@@ -1,17 +1,9 @@
-const removeDuplicates = (nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) => {
-  arrLength = nums.length;
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i === j) continue;
-      if (nums[i] === nums[j]) {
-        nums.splice(j, 1);
-      }
+const removeDuplicates = (nums) => {
+  let count = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== nums[j + 1]) {
+      nums[count++] = nums[j];
     }
   }
-  const k = [...nums];
-  const count = arrLength - nums.length;
-  for (let k = 0; k < count; k++) {
-    nums.push("_");
-  }
-  return `${k.length}, nums = ${nums}`;
+  return count;
 };
