8d268c5951bb86cff3e76e4cdb0f88f8158149e7
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-09-17 14:12:36 +0100

    Refractore Day 5 challenge, added new snapshot

--- Zeuhz/Day 5 -Plus One/plusOne.js
+++ Zeuhz/Day 5 -Plus One/plusOne.js
@@ -1,8 +1,4 @@
 const plusOne = (digits) => {
-  if (digits.length > 15) {
-    const num = "" + (BigInt(digits.join("")) + 1n);
-    return num.split("");
-  }
-  const num = "" + (+digits.join("") + 1);
+  const num = "" + (BigInt(digits.join("")) + 1n);
   return num.split("");
 };
