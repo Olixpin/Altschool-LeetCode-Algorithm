28bd5003e1563785de451174c935bb5d1be92cfe
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-09-17 11:03:30 +0100

    Renamed folders

--- Zeuhz/removeDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.js
+++
@@ -1,9 +0,0 @@
-const removeDuplicates = (nums) => {
-  let count = 0;
-  for (let j = 0; j < nums.length; j++) {
-    if (nums[j] !== nums[j + 1]) {
-      nums[count++] = nums[j];
-    }
-  }
-  return count;
-};
