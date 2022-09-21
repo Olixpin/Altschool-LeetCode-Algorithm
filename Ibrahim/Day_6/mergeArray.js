68325fa7d7371ea958080c929bd4828fb79f6009
Oliyide Ibrahim Owolabi <oliyideibrahim123@gmail.com>
2022-09-21 01:00:17 +0100

    added comment for better understing--Ibrahim folder

--- Ibrahim/Day_6/mergeArray.js
+++ Ibrahim/Day_6/mergeArray.js
@@ -7,8 +7,11 @@
  */
 
  var merge = function(nums1, m, nums2, n) {
+    // splice to remove the zeros
     nums1.splice(m,n)
+    // push array nuw2 to nums1 by using soread operator
     nums1.push(...nums2)
+    // use sort method to sort the array in ascending order
     nums1.sort()
 
 };
