be5e89f181e538a30206af3854edb3fc5fd6fd11
github_adebami7 <fiyinfoluwademide@gmail.com>
2022-09-21 19:34:20 +0100

    New-assignment-added

---
+++ ADEMIDE/Arrays/day-6/merge-sorted-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    var i = m - 1;
+    var j = n - 1;
+    var k = m + n - 1;
+    while (j >= 0) {
+      nums1[k--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+    }
+  };
